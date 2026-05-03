696256db6fa0336753bcd84145f6359de0a82b03
Utsav Shahi <92775677+shahiutsav@users.noreply.github.com>
2026-05-03 09:01:34 +0545

    Use biome as formatter

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -1,17 +1,3 @@
-import Root, {
-	type ButtonProps,
-	type ButtonSize,
-	type ButtonVariant,
-	buttonVariants,
-} from "./button.svelte";
+import Root, { type ButtonProps, type ButtonSize, type ButtonVariant, buttonVariants } from "./button.svelte";
 
-export {
-	Root,
-	type ButtonProps as Props,
-	//
-	Root as Button,
-	buttonVariants,
-	type ButtonProps,
-	type ButtonSize,
-	type ButtonVariant,
-};
+export { type ButtonProps, type ButtonProps as Props, type ButtonSize, type ButtonVariant, buttonVariants, Root as Button, Root };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
   import ArrowUpIcon from "@lucide/svelte/icons/arrow-up";
+  import { invoke } from "@tauri-apps/api/core";
   import { Button } from "$lib/components/ui/button/index.js";
 
   let name = $state("");
@@ -17,21 +17,10 @@
   <h1>Welcome to Tauri + Svelte</h1>
 
   <div class="row">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
+    <form class="row" onsubmit={greet}>
+      <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+      <Button type="submit">Greet</Button>
+    </form>
+    <p>{greetMsg}</p>
   </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <Button type="submit">Greet</Button>
-  </form>
-  <p>{greetMsg}</p>
 </main>
